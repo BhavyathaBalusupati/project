fc053c51ac19f1c88a70dda2a30a7be501f31df4
BhavyathaBalusupati <bhavyathabalusupati@gmail.com>
2026-06-27 10:32:11 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
+    apiKey: "AIzaSyDkLV43v0FzT31VHECdF23Qexwv92TrSrM",
     authDomain: "digital-issue-cart-management.firebaseapp.com",
     projectId: "digital-issue-cart-management",
     storageBucket: "digital-issue-cart-management.appspot.com",
